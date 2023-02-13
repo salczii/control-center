d0b7a357db4d04a39da61718ef2a31943756ab3c
salczii <kacper.salek32@gmail.com>
2023-02-13 11:56:58 +0100

    clean up Index file and add _document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <p className={"text-4xl"}>Hey it is Taiwlind Css</p>
       <Component {...pageProps} />
     </div>
   );

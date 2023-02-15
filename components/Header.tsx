3e98a5b1a81b3bf9807daa38d65f91c50b24e064
salczii <kacper.salek32@gmail.com>
2023-02-15 23:34:08 +0100

    add a basic layout to the app

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export function Header() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
   return (
     <div>
       <header className={"max-w-md mx-auto w-full text-amber-50"}>
-        <nav className={"bg-gray-900 px-4 py-2"}>Navigation</nav>
+        <nav className={"bg-gray-900 px-4 py-2"}>
+          <Link href="/" className={`${currentRoute === "/" && "selected"}`}>
+            Home
+          </Link>
+          <Link
+            href="/details"
+            className={`${currentRoute === "/details" && "selected"}`}
+          >
+            details
+          </Link>
+          <Link
+            href="tasks"
+            className={`${currentRoute === "/tasks" && "selected"}`}
+          >
+            tasks
+          </Link>
+        </nav>
       </header>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,38 @@ const Home = () => {
   return (
     <div className={"flex flex-col min-h-screen bg-blue-50"}>
       <Header />
-      <main className={"flex-grow"}>Content</main>
+      <main className={"flex-grow mx-auto flex flex-col"}>
+        <div className="test">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          aliquam asperiores aspernatur autem, consequatur delectus esse harum
+          in ipsum libero magni, non numquam praesentium quisquam, rem
+          repudiandae sed soluta voluptate
+        </div>
+        <div className="test">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          aliquam asperiores aspernatur autem, consequatur delectus esse harum
+          in ipsum libero magni, non numquam praesentium quisquam, rem
+          repudiandae sed soluta voluptate
+        </div>
+        <div className="test">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          aliquam asperiores aspernatur autem, consequatur delectus esse harum
+          in ipsum libero magni, non numquam praesentium quisquam, rem
+          repudiandae sed soluta voluptate
+        </div>
+        <div className="test">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          aliquam asperiores aspernatur autem, consequatur delectus esse harum
+          in ipsum libero magni, non numquam praesentium quisquam, rem
+          repudiandae sed soluta voluptate
+        </div>
+        <div className="test">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          aliquam asperiores aspernatur autem, consequatur delectus esse harum
+          in ipsum libero magni, non numquam praesentium quisquam, rem
+          repudiandae sed soluta voluptate
+        </div>
+      </main>
       <Footer />
     </div>
   );
